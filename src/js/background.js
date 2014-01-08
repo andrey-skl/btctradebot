8204fb013d0550a46aadf898a6df879cefaac28f
huston007 <andrey.skladchikov@gmail.com>
2014-01-08 20:05:11 +0600

    deleted file

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,6 @@
 var btceApi = new btceAPI({
 	key: "TBESLB4S-3ZKGN7TR-XGCCOVG7-LW0IOYL6-LHOCSP3I",
-	secret: "4dd88305e97a4a80ff95b77101af1980f5905e918ecdbeb7bea3e779c64489a7",
+	secret: "YOURSECRET",
 	nonce:3,
 });
 
